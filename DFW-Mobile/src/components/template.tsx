33cf5be1285e1985272768475f58bee5b5f5802c
TheHypernova <saturnmfgg@gmail.com>
2020-10-13 23:56:14 -0500

    Added template (as a start)

---
+++ DFW-Mobile/src/components/template.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Template {
+    head = (
+        <div key='h_'className='header'>
+            <div key='h_logo' className='logo'>
+                {`LOGO GOES HERE`}
+            </div>
+        </div>
+    );
+
+    content = (
+        <div className='content'>
+            
+        </div>
+    );
+}
