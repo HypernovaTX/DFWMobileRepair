e29d2b91da7af54f8181a61addc776a022080cc8
TheHypernova <saturnmfgg@gmail.com>
2020-10-15 23:55:00 -0500

    Some size changes

--- DFW-Mobile/src/components/template.tsx
+++ DFW-Mobile/src/components/template.tsx
@@ -19,9 +19,9 @@ export default class Template {
                             onDragStart={preventDrag}
                         ></img>
                         <div key='h_logo_button_section' className='land-btn-section'>
-                            <span key='hl_btn_1' className='land-btn'>CONTACT US</span>
                             <span key='hl_btn_2' className='land-item'>ABOUT US</span>
                             <span key='hl_btn_3' className='land-item'>SERVICES</span>
+                            <span key='hl_btn_1' className='land-btn'>CONTACT US</span>
                         </div>
                     </div>
                 </div>
@@ -31,19 +31,27 @@ export default class Template {
 
     static content(): JSX.Element {
         return (
-            <div className='content'>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
-                <br></br><br></br><br></br>
+            <div key='content' className='content'>
+                <div key='section1' className='ct-section section1'>
+                    <div key='about_us' className='section1-box'>
+                        <h3>About US</h3>
+                        <p>
+                            DFW Mobile Repair - Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. Ut enim 
+                            ad minim veniam, quis nostrud exercitation ullamco 
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                            aute irure dolor in reprehenderit in voluptate velit 
+                            esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                            sint occaecat cupidatat non proident, sunt in culpa 
+                            qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+
+                <div key='section2' className='ct-section section2'>
+
+                </div>
             </div>
         );
     }
