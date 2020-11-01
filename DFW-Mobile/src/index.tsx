373b4ff4ead78a2aacf5c5e5c5f19797c4d7feb9
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2020-11-01 01:33:38 -0500

    Added mobile theme

--- DFW-Mobile/src/index.tsx
+++ DFW-Mobile/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './resources/mobile.css';
 import * as serviceWorker from './serviceWorker';
 import MainPage from './components/mainpage';
 
