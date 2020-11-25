f0f1354a13e2fdb5fce839d5cab09276bb834bd2
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2020-11-24 22:50:07 -0600

    User css

--- DFW-Mobile/src/components/user.tsx
+++ DFW-Mobile/src/components/user.tsx
@@ -24,7 +24,9 @@ export default class User extends React.Component<Props, State> {
 
         return (<div key='userbar_main' className='user-bar'>
             <div key='userbar_icon' className='user-icon'>
-                {dis_user}
+                <div key='user_text' className='user-text'>
+                    {dis_user}
+                </div>
                 <div key='user_hamburger' className='user-hamburber'>
                     <div key='hbu_1' className='user-hamburger-dash'></div>
                     <div key='hbu_2' className='user-hamburger-dash'></div>
