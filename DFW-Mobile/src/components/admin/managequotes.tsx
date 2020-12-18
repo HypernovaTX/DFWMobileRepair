bd57d26b0c89cc04fc7f7aef73c8fb7b02560c30
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2020-12-17 23:20:51 -0600

    Some functions in Quote class

---
+++ DFW-Mobile/src/components/admin/managequotes.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+import * as CONFIG from '../../config.json';
+
+type Props = {
+    loggedIn: boolean;
+};
+type State = {
+    list: object,
+    selection: object,
+};
+
+
+export default class ManageQuotes extends React.Component<Props, State> {
+    constructor(p: Props) {
+        super(p);
+
+        this.state = {
+            list: {},
+            selection: {},
+        }
+    }
+
+    getYears(): void {
+        axios.get(`${CONFIG.backendhost}/${CONFIG.backendindex}?act=quote&u=year`)
+            .then((response) => {
+                let responseString = response.data.split(',');
+                //this.setState({});
+            });
+    };
+
+    template(): JSX.Element {
+        return <div key='mq_body' className='mq-body'></div>;
+    }
+
+    render() {
+        return this.template();
+    }
+}
