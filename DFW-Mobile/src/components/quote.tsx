f91afb95c6f62f392f15e97e3fca9a5bf73c6f46
ChengAn (Arthur) Guo <saturnmfgg@gmail.com>
2021-01-20 21:15:14 -0600

    Getting started with public quote

---
+++ DFW-Mobile/src/components/quote.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+import * as CONFIG from '../config.json';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faPlus, faUsers, faTags } from '@fortawesome/free-solid-svg-icons';
+//import Cookies from 'js-cookie';
+
+type Props = {
+};
+type State = {
+};
+
+export default class Quotes extends React.Component<Props, State> {
+    constructor(p: Props) {
+        super(p);
+    }
+
+    render() {
+        return(<React.Fragment key='na'></React.Fragment>);
+    }
+}
